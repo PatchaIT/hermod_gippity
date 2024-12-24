252b76aaf83be9e3712deec8b9d525db7cc5ff49
Patcha <outpatcha@outlook.it>
2024-12-24 20:31:54 +0100

    0.0.1-Skeleton2
    Added SonarCloud support and solved all issues and coverages.
    Updated all dependency versions to last stable.

--- src/test/java/it/patcha/hermod/gpt/HermodGippityTest.java
+++ src/test/java/it/patcha/hermod/gpt/HermodGippityTest.java
@@ -1,39 +1,39 @@
 package it.patcha.hermod.gpt;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+class HermodGippityTest {
+	private static final Logger logger = (Logger) LoggerFactory.getLogger(HermodGippity.class);
+	private ListAppender<ILoggingEvent> listAppender;
 
-import org.junit.jupiter.api.Test;
-
-public class HermodGippityTest {
-
-	@Test
-	public void testMainWithoutArgument() {
-		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(outContent));
-
-		HermodGippity.main(new String[]{});
-		String output = outContent.toString().trim();
-
-		System.setOut(null);
-
-		assertEquals("This is a stub, you put no argument", output);
+	@BeforeEach
+	void setUp() {
+		listAppender = new ListAppender<>();
+		listAppender.start();
+		logger.addAppender(listAppender);
 	}
 
 	@Test
-	public void testMainWithArgument() {
-		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(outContent));
+	void testMainWithoutArgument() {
+		HermodGippity.main(new String[]{});
+		ILoggingEvent loggingEvent = listAppender.list.get(0);
+		String logMessage = loggingEvent.getFormattedMessage();
+		assertEquals("This is a stub, you put no argument", logMessage);
+	}
 
+	@Test
+	void testMainWithArgument() {
 		String argument = "argument";
 		HermodGippity.main(new String[]{argument});
-		String output = outContent.toString().trim();
-
-		System.setOut(null);
-
-		assertEquals("This is a stub, your argument is: " + argument, output);
+		ILoggingEvent loggingEvent = listAppender.list.get(0);
+		String logMessage = loggingEvent.getFormattedMessage();
+		assertEquals("This is a stub, your argument is: " + argument, logMessage);
 	}
-
 }
